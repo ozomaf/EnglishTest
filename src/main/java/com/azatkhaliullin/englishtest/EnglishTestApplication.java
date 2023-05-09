10dd523e77faa2a1d945bd9cd61d6fe50dc11d41
azatkhaliyllin <khaliyllin56@gmail.com>
2023-05-09 19:13:14 +0400

    initial commit

---
+++ src/main/java/com/azatkhaliullin/englishtest/EnglishTestApplication.java
@@ -0,0 +1,13 @@
+package com.azatkhaliullin.englishtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/azatkhaliullin/englishtest/EnglishTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azatkhaliullin.englishtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
