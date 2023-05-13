2e77f3f3ba2f6d0701afaf4084c54ee404ea9210
azatkhaliyllin <khaliyllin56@gmail.com>
2023-05-13 22:41:49 +0400

    fixed connection to the database during the test

--- src/test/java/com/azatkhaliullin/englishtest/EnglishTestApplicationTests.java
+++ src/test/java/com/azatkhaliullin/englishtest/EnglishTestApplicationTests.java
@@ -1,10 +1,21 @@
 package com.azatkhaliullin.englishtest;
 
+import com.azatkhaliullin.englishtest.data.AnswerRepository;
+import com.azatkhaliullin.englishtest.data.EnglishLevelRepository;
+import com.azatkhaliullin.englishtest.data.EnglishTestRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@WebMvcTest
 class EnglishTestApplicationTests {
+
+    @MockBean
+    EnglishTestRepository englishTestRepo;
+    @MockBean
+    EnglishLevelRepository englishLevelRepo;
+    @MockBean
+    AnswerRepository answerRepo;
 
     @Test
     void contextLoads() {
